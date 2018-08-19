7c3f11e6ab1f0808ebc44f94b35870cff083ec56
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-08-19 21:00:26 +0200

    add code and web icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-<div>
-  <header class="header container">
-    <h1 class="h1">Jakub Jabłoński</h1>
-    <p class="p">Hi! I'm a highly motivated, self-starting developer seeking to launch a career building web applications and services.</p>
-    <p class="p">I love creating complex user interfaces using modern JavaScript techniques and frameworks.</p>
-    <p class="p"><span class="heart">&hearts;&nbsp;</span> Vue, Node, Progressive Web Apps <span class="heart">&nbsp;&hearts;</span></p> 
-  </header>
-  <main class="main container">
-    <h2 class="h2">Projects</h2>
-    <project-grid/>
-  </main>
-</div>
+  <div>
+    <header class="header container">
+      <h1 class="h1">Jakub Jabłoński</h1>
+      <p class="p">Hi! I'm a highly motivated, self-starting developer seeking to launch a career building web applications and services.</p>
+      <p class="p">I love creating complex user interfaces using modern JavaScript techniques and frameworks.</p>
+      <p class="p"><span class="heart">&hearts;&nbsp;</span> Vue, Node, Progressive Web Apps <span class="heart">&nbsp;&hearts;</span></p> 
+    </header>
+    <main class="main container">
+      <h2 class="h2">Projects</h2>
+      <project-grid/>
+    </main>
+  </div>
 </template>
 
 <script>
--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="container projects">
-    <h1 class="h1" slot="heading">{{ project.name }}</h1>
-    <p>{{ project.description }}</p>      
+  <div class="container project">
+    <div class="project__info">
+      <header class="project__header">
+        <h1 class="h1 project__heading">{{ project.name }}</h1>
+        <icon-globe class="project__icon"/>
+        <icon-code  class="project__icon"/>
+      </header>
+      <p>{{ project.description }}</p> 
+    </div>
     <img :src="project.images.main" alt="">
   </div>
 </template>
 
 <script>
+import IconGlobe from '@/assets/svg/globe.svg'
+import IconCode from '@/assets/svg/code.svg'
+
 export default {
   transition: 'oko',
+  components: {
+    IconGlobe,
+    IconCode
+  },
   computed: {
     project() {
       return this.$store.getters.projectById(this.$route.params.id);
@@ -19,32 +32,50 @@ export default {
 
 
 <style lang="scss" scoped>
-.projects {
+.project {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 10rem;
+
+  &__header {
+    display: flex;
+  }
+
+  &__heading {
+    margin-right: auto;
+  }
+
+  &__icon {
+    width: 3.5rem;
+    height: 3.5rem;
+    fill: $color-grey-light-1;
+
+    &:not(:last-child) {
+      margin-right: 1.5rem;
+    }
+  }
 }
 
 img {
   width: 100%;
 }
-.projects {
-  // padding: 3rem 0;
-  // display: flex;
-  // align-items: flex-end;
+// .projects {
+//   // padding: 3rem 0;
+//   // display: flex;
+//   // align-items: flex-end;
 
-  &__divider {
-    width: 20rem;
-    height: 2px;
-    background-color: $color-grey-light-1;
-    margin-bottom: 2rem;
-  }
+//   &__divider {
+//     width: 20rem;
+//     height: 2px;
+//     background-color: $color-grey-light-1;
+//     margin-bottom: 2rem;
+//   }
 
-  &__content {
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-}
+//   &__content {
+//     height: 50%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//   }
+// }
 </style>
