07c53972c32df787a6fd05559c36352b0c16ba99
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-08-19 16:59:31 +0200

    improve aboutme text

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
   <header class="header container">
     <h1 class="h1">Jakub Jabłoński</h1>
     <p class="p">Hi! I'm a highly motivated, self-starting developer seeking to launch a career building web applications and services.</p>
-    <p class="p">I love building complex user interfaces using modern JavaScript techniques and frameworks.</p>
-    <p class="p"><span class="heart">&hearts;</span> Vue, Node, Progressive Web Apps <span class="heart">&hearts;</span></p> 
+    <p class="p">I love creating complex user interfaces using modern JavaScript techniques and frameworks.</p>
+    <p class="p"><span class="heart">&hearts;&nbsp;</span> Vue, Node, Progressive Web Apps <span class="heart">&nbsp;&hearts;</span></p> 
   </header>
   <main class="main container">
     <h2 class="h2">Projects</h2>
