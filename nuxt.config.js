5a207a0c500fdb32df32bde74c34f73af8086bc3
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-08-27 20:09:32 +0200

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,11 +24,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Jakub Jabłoński Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Personal protfolio of web developer Jakub Jabłoński.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
